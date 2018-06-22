6e9e3575d05d88ebc9688300c47d0afa7810d1fe
Josh Zeilmann <jzeilmann93@gmail.com>
2018-06-21 21:39:12 -0500

    Scss

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
+$.fn.toggleClick = function(){
+    var methods = arguments, // store the passed arguments for future reference
+        count = methods.length; // cache the number of methods 
+
+    //use return this to maintain jQuery chainability
+    return this.each(function(i, item){
+        // for each element you bind to
+        var index = 0; // create a local counter for that element
+        $(item).click(function(){ // bind a click handler to that element
+            return methods[index++ % count].apply(this,arguments); // that when called will apply the 'index'th method to that element
+            // the index % count means that we constrain our iterator between 0 and (count-1)
+        });
+    });
+};
+
+
 $(function () {
     // cache the sliding object in a var
     var slideout = $('#aside');
+    console.log(slideout);
     // "click-me" is what is in your html not "clickme"
-    $("#menu-icon").toggle(function () {
+    $("#menuIcon").toggleClick(function () {
+        console.log("clicked");
         // use cached object instead of searching
         slideout.animate({
             right: '0px'
