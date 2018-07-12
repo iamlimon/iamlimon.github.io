0aeb28fbdad2f23454ae349e4bd71f569bd16815
Josh Zeilmann <jzeilmann93@gmail.com>
2018-07-11 21:43:12 -0500

    Clean up mouse over functiions

--- js/main.js
+++ js/main.js
@@ -15,13 +15,9 @@ $.fn.toggleClick = function(){
 
 
 $(function () {
-    // cache the sliding object in a var
     var slideout = $('.navigation');
-    console.log(slideout);
-    // "click-me" is what is in your html not "clickme"
     $(".menu").toggleClick(function () {
-        console.log("clicked");
-        // use cached object instead of searching
+        slideout.css('display', 'block');
         slideout.animate({
             top: '96',
             opacity: '1'
@@ -30,7 +26,6 @@ $(function () {
             duration: 200
         });
     }, function () {
-        // use cached object instead of searching        
         slideout.animate({
             top: '-183',
             opacity: '0'
@@ -41,20 +36,22 @@ $(function () {
     });
 });
 
-$(".menu").mouseover(function() {
-    $("#line2").css('margin-left', '10px');
-    console.log("ya hovered.")
-});
-$(".menu").mouseout(function() {
-    $("#line2").css('margin-left', '0');
-    console.log("ya hovered.")
-});
+//Menu Hover Animation
+$('.menu').hover(
+    function() {
+        $("#line2").css('margin-left', '10px');
+    }, 
+    function() {
+        $("#line2").css('margin-left', '0px');
+    }
+)
 
-$(".nav-svg").mouseover(function() {
-    $(this).next().css('visibility', 'visible');
-    console.log("FOO");
-});
-$(".nav-svg").mouseout(function() {
-    $(this).next().css('visibility', 'hidden');
-    console.log("BAR");
-});
+//Show nav tooltips on icon hover
+$('.nav-svg').hover(
+    function() {
+        $(this).next().css('visibility', 'visible');
+    }, 
+    function() {
+        $(this).next().css('visibility', 'hidden');
+    }
+)
